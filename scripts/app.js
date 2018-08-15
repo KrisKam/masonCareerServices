32deed1e24d8c7255167068b9e4181f3cfce9efa
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-08-15 17:03:29 -0600

    Add dynamically created table cells with API data

--- scripts/app.js
+++ scripts/app.js
@@ -1,35 +1,59 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  
+
   const submit = document.querySelector("#infoSubmit");
 
   submit.addEventListener("click", function (event) {
     event.preventDefault();
-    
+
     const industry = document.querySelectorAll("input[name='industryRadio']");
-  
     const companySize = document.querySelectorAll("input[name='sizeRadio']");
 
-for (let i = 0; i < industry.length; i++) {
-  if (industry[i].checked) {
-    console.log(industry[i]["value"])
-  }
-}
+    const industryValue = assignValue(industry);
+    const sizeValue = assignValue(companySize)
 
-    // if (document.querySelectorAll("input[name='industryRadio']").checked) {
-    //   let a =document.querySelectorAll("input[name='industryRadio']")
-    //   console.log("a is equal to :", a)
-    // }
-    // console.log("industry: ", industry.checked)
-    // console.log("size: ", companySize.value);
-    
+    function assignValue(radioInput) {
+      for (let i = 0; i < radioInput.length; i++) {
+        if (radioInput[i].checked) {
+          return radioInput[i]["value"];
+        }
+      }
+    }
 
 
-    const description = document.querySelector("#description")
-
-    axios.get(`https://api-v2.themuse.com/companies?api_key=3e6d0622728f87213ce3f2fb40fd7eae7a99abb95cc75fa1b04f0d159639954f&industry=${industry.value}&page=1`)
+    axios.get(`https://api-v2.themuse.com/companies?api_key=3e6d0622728f87213ce3f2fb40fd7eae7a99abb95cc75fa1b04f0d159639954f&industry=${industryValue}&size=${sizeValue}&page=1`)
       .then(result => {
-        // description.textContent = result["data"]["results"][0]["description"];
-        console.log("result:  ", result)
+        const resultArray = result["data"]["results"];
+        const tableBody = document.querySelector("tbody");
+        if (resultArray.length > 0) {
+          resultArray.forEach(item => {
+            const newRow = document.createElement("tr");
+            tableBody.appendChild(newRow);
+            const newCell = document.createElement("td");
+            newCell.textContent = item["name"];
+            newRow.appendChild(newCell);
+            const newCell2 = document.createElement("td");
+            newCell2.textContent = locations(item["locations"]);
+            newRow.appendChild(newCell2);
+            const newCell3 = document.createElement("td");
+            newCell3.textContent = item["description"];
+            newRow.appendChild(newCell3);
+
+            function locations(arr) {
+              let locationList = "";
+              arr.forEach(location => {
+                locationList += location["name"]
+              })
+              console.log("arr: ", locationList)
+              return locationList;
+            }
+          })      
+        } else {
+          const newRow = document.createElement("tr");
+          tableBody.appendChild(newRow);
+          const newCell = document.createElement("td");
+          newCell.textContent = "No data availabe for this industry.";
+          newRow.appendChild(newCell);
+        }
       })
       .catch(result => {
         console.log(result.response)
